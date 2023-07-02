6c10b1421b69b87b5e4f66b9695400371b5254a4
胥天昊 <1499487526@qq.com>
2023-07-02 17:43:14 +0800

    初始化listAssign

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -1,10 +1,17 @@
 // 封装管理员相关接口
 import request from "@/utils/http";
 
-// 获取所有未指派的异常信息
+// 获取所有异常信息
 export const listAllUnassignedExInfo = () => {
   return request({
     url: "/admin",
+    method: "GET",
+  });
+};
+// 获取所有已经指派给监督员的异常信息
+export const listAssigned = () => {
+  return request({
+    url: "/admin/getMessage",
     method: "GET",
   });
 };
--- src/views/ListAssign/index.vue
+++ src/views/ListAssign/index.vue
@@ -1,39 +1,38 @@
 <style></style>
 <template>
   <el-table :data="tableData" stripe style="width: 100%">
-    <el-table-column prop="date" label="日期" width="180"> </el-table-column>
-    <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+    <el-table-column prop="id" label="id" width="180"> </el-table-column>
+    <el-table-column prop="updateTime" label="日期" width="180">
+    </el-table-column>
+    <el-table-column prop="status" label="status" width="180">
+    </el-table-column>
+    <el-table-column prop="supervisorId" label="supervisorId" width="180">
+    </el-table-column>
+
+    <el-table-column prop="vAQILevel" label="vAQILevel" width="100">
+    </el-table-column>
+    <el-table-column prop="description" label="姓名" width="180">
+    </el-table-column>
     <el-table-column prop="address" label="地址"> </el-table-column>
   </el-table>
 </template>
 
 <script>
+import { listAssigned } from "@/apis/admin.js";
 export default {
   data() {
     return {
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄",
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄",
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄",
-        },
-      ],
+      tableData: [],
     };
+  },
+  async mounted() {
+    const data = await listAssigned();
+    console.log(data);
+    // 2023-07-02T07:23:51、
+    for (let i = 0; i < data.data.length; i++) {
+      data.data[i].updateTime = data.data[i].updateTime.substring(0, 19);
+    }
+    this.tableData = data.data;
   },
 };
 </script>
