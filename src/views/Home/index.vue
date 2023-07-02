29a59c298b154aa56091d789d84089d7e95e1503
胥天昊 <1499487526@qq.com>
2023-07-02 21:07:24 +0800

    初始化echarts

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,109 +1,47 @@
 <template>
-  <v-chart class="chart" :option="option" autoresize />
+  <div class="Echarts">
+    <div id="main" style="width: 600px; height: 400px"></div>
+  </div>
 </template>
 
 <script>
-import VChart from "vue-echarts";
-import { ref, defineComponent } from "vue";
+export default {
+  name: "Echarts",
+  methods: {
+    myEcharts() {
+      // 基于准备好的dom，初始化echarts实例
+      var myChart = this.$echarts.init(document.getElementById("main"));
 
-export default defineComponent({
-  name: "HelloWorld",
-  components: {
-    VChart,
-  },
-
-  setup() {
-    const option = ref({
-      title: {
-        text: "您最近的提交工程量",
-        subtext: "Fake Data",
-        left: "center",
-      },
-
-      legend: {
-        left: "center",
-        top: "bottom",
-        data: [
-          "rose1",
-          "rose2",
-          "rose3",
-          "rose4",
-          "rose5",
-          "rose6",
-          "rose7",
-          "rose8",
+      // 指定图表的配置项和数据
+      var option = {
+        title: {
+          text: "ECharts 入门示例",
+        },
+        tooltip: {},
+        legend: {
+          data: ["销量"],
+        },
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [5, 20, 36, 10, 10, 20],
+          },
         ],
-      },
-      toolbox: {
-        show: true,
-        feature: {
-          mark: { show: true },
-          dataView: { show: true, readOnly: false },
-          restore: { show: true },
-          saveAsImage: { show: true },
-        },
-      },
-      series: [
-        // 第一个图片
-        {
-          name: "Radius Mode",
-          type: "pie",
-          radius: [20, 140],
-          center: ["25%", "50%"],
-          roseType: "radius",
-          itemStyle: {
-            borderRadius: 5,
-          },
-          label: {
-            show: false,
-          },
-          emphasis: {
-            label: {
-              show: true,
-            },
-          },
-          data: [
-            { value: 40, name: "rose 1" },
-            { value: 33, name: "rose 2" },
-            { value: 28, name: "rose 3" },
-            { value: 22, name: "rose 4" },
-            { value: 20, name: "rose 5" },
-            { value: 15, name: "rose 6" },
-            { value: 12, name: "rose 7" },
-            { value: 10, name: "rose 8" },
-          ],
-        },
-        // 第二个
-        {
-          name: "Area Mode",
-          type: "pie",
-          radius: [20, 140],
-          center: ["75%", "50%"],
-          roseType: "area",
-          itemStyle: {
-            borderRadius: 5,
-          },
-          data: [
-            { value: 30, name: "rose 1" },
-            { value: 28, name: "rose 2" },
-            { value: 26, name: "rose 3" },
-            { value: 24, name: "rose 4" },
-            { value: 22, name: "rose 5" },
-            { value: 20, name: "rose 6" },
-            { value: 18, name: "rose 7" },
-            { value: 16, name: "rose 8" },
-          ],
-        },
-      ],
-    });
+      };
 
-    return { option };
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+    },
   },
-});
+  mounted() {
+    this.myEcharts();
+  },
+};
 </script>
 
-<style scoped>
-.chart {
-  height: 100vh;
-}
-</style>
+<style></style>
