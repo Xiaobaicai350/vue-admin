fc9ab813a8b6a1196c8b908fd4e31279b6d48f1f
胥天昊 <1499487526@qq.com>
2023-07-02 17:22:27 +0800

    m

--- src/views/ListExInformation/index.vue
+++ src/views/ListExInformation/index.vue
@@ -1,85 +1,89 @@
 <template>
-  <el-table :data="tableData" style="width: 100%">
-    <el-table-column label="日期" width="180">
-      <template slot-scope="scope">
-        <i class="el-icon-time"></i>
-        <span style="margin-left: 10px">{{ scope.row.date }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column label="姓名" width="180">
-      <template slot-scope="scope">
-        <el-popover trigger="hover" placement="top">
-          <p>姓名: {{ scope.row.name }}</p>
-          <p>住址: {{ scope.row.address }}</p>
-          <div slot="reference" class="name-wrapper">
-            <el-tag size="medium">{{ scope.row.name }}</el-tag>
+  <div>
+    <el-table :data="tableData" stripe style="width: 100%">
+      <el-table-column prop="updateTime" label="日期" width="180">
+      </el-table-column>
+      <el-table-column prop="supervisorId" label="举报人姓名" width="180">
+      </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+      <el-table-column prop="vAQILevel" label="预测AQI等级"> </el-table-column>
+      <el-table-column prop="status" label="状态"> </el-table-column>
+      <el-table-column prop="description" label="描述"> </el-table-column>
+
+      <!-- 最后一列 -->
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="primary"
+            @click="handleAssign(scope.$index, scope.row)"
+          >
+            委派
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <!-- 弹窗 -->
+    <template>
+      <div>
+        <el-dialog title="委派" :visible.sync="dialogFormVisible">
+          <el-form :model="form">
+            <el-form-item label="委派事件id" :label-width="formLabelWidth">
+              <el-input v-model="form.name" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="选择委派人名称" :label-width="formLabelWidth">
+              <el-select v-model="form.region" placeholder="请选择活动区域">
+                <el-option label="区域一" value="shanghai"></el-option>
+                <el-option label="区域二" value="beijing"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisible = false">取 消</el-button>
+            <el-button type="primary" @click="dialogFormVisible = false"
+              >确 定</el-button
+            >
           </div>
-        </el-popover>
-      </template>
-    </el-table-column>
-    <el-table-column label="操作">
-      <template slot-scope="scope">
-        <el-button
-          size="mini"
-          type="primary"
-          @click="handleAssign(scope.$index, scope.row)"
-        >
-          委派
-        </el-button>
-        <Dialog v-if="assignDialog" />
-        <el-button
-          size="mini"
-          type="danger"
-          @click="handleDelete(scope.$index, scope.row)"
-          >删除</el-button
-        >
-      </template>
-    </el-table-column>
-  </el-table>
+        </el-dialog>
+      </div>
+    </template>
+  </div>
 </template>
 
 <script>
-import Dialog from "./components/Dialog.vue";
+import { listAllUnassignedExInfo } from "@/apis/admin.js";
 export default {
   data() {
     return {
-      // 为false不显示，为ture显示
-      assignDialog: false,
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄",
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄",
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄",
-        },
-      ],
+      tableData: [],
+
+      dialogFormVisible: false,
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: "",
+      },
+      formLabelWidth: "120px",
     };
   },
   methods: {
     handleAssign(index, row) {
-      this.assignDialog = true;
-      console.log(index, row);
-    },
-    handleDelete(index, row) {
+      this.dialogFormVisible = true;
       console.log(index, row);
     },
   },
-  components: {
-    Dialog,
+  components: {},
+  async mounted() {
+    const data = await listAllUnassignedExInfo();
+    console.log(data);
+    this.tableData = data.data;
+    console.log("我是ListExInformation的mounted方法");
   },
 };
 </script>
