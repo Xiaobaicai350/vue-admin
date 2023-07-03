b754f30b922fad9eb76317217026f75b5a72f285
胥天昊 <1499487526@qq.com>
2023-07-03 21:11:41 +0800

    ,m

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -1,14 +1,19 @@
 // 封装管理员相关接口
 import request from "@/utils/http";
 
-// 获取所有异常信息
-export const listAllUnassignedExInfo = () => {
+//admin登录
+export const login = ({ id, password }) => {
   return request({
-    url: "/admin",
-    method: "GET",
+    url: "/admin/login",
+    method: "POST",
+    data: {
+      id,
+      password,
+    },
   });
 };
-// 获取所有异常信息
+
+// 获取所有委派信息
 export const listAssigned = () => {
   return request({
     url: "/admin/getMessage",
@@ -23,19 +28,32 @@ export const listAssignedInfo = () => {
     method: "GET",
   });
 };
-//监督员登录
-export const login = ({ id, password }) => {
+
+// 把异常信息指派给检测员
+export const postExToAQI = () => {
   return request({
-    url: "/admin/login",
+    url: "/admin/giveStaff/:exId/:aqiId",
     method: "POST",
-    data: {
-      id,
-      password,
-    },
   });
 };
 
-//监督员登录
+//获取全部的检测员
+export const getStaff = () => {
+  return request({
+    url: "/admin/getStaff",
+    method: "GET",
+  });
+};
+
+//获取检测员检测后的数据
+export const testResult = () => {
+  return request({
+    url: "/admin/result",
+    method: "GET",
+  });
+};
+
+//admin信息
 export const info = () => {
   return request({
     url: "/admin/info",
