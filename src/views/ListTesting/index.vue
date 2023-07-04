5c1cdcd825b03e73dffe85f110ca870e612fc93b
胥天昊 <1499487526@qq.com>
2023-07-04 09:21:29 +0800

    m

--- src/views/ListTesting/index.vue
+++ src/views/ListTesting/index.vue
@@ -2,17 +2,22 @@
 <template>
   <el-table :data="tableData" stripe style="width: 100%">
     <el-table-column prop="id" label="id"> </el-table-column>
-    <el-table-column prop="address" label="address"> </el-table-column>
-    <el-table-column prop="description" label="description"> </el-table-column>
+    <el-table-column prop="exMessage.address" label="address">
+    </el-table-column>
+    <el-table-column prop="exMessage.description" label="description">
+    </el-table-column>
     <el-table-column prop="publicName" label="publicName"> </el-table-column>
     <el-table-column prop="staffName" label="staffName"> </el-table-column>
     <el-table-column prop="updateTime" label="updateTime"> </el-table-column>
-    <el-table-column prop="vAQILevel" label="vAQILevel"> </el-table-column>
+    <el-table-column prop="aqilevel" label="aqilevel"> </el-table-column>
+    <el-table-column prop="CO" label="CO"> </el-table-column>
+    <el-table-column prop="PM" label="PM2.5"> </el-table-column>
+    <el-table-column prop="SO2" label="SO2"> </el-table-column>
   </el-table>
 </template>
 
 <script>
-import { listAssignedInfo } from "@/apis/admin.js";
+import { testResult } from "@/apis/admin.js";
 export default {
   data() {
     return {
@@ -20,7 +25,7 @@ export default {
     };
   },
   async mounted() {
-    const data = await listAssignedInfo();
+    const data = await testResult();
     console.log(data);
     for (let i = 0; i < data.data.length; i++) {
       data.data[i].updateTime = data.data[i].updateTime.substring(0, 19);
